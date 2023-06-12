c973d333a43cff4005170e641085ad6bc48197f5
Andy Song <wsong.cn@gmail.com>
2023-06-12 12:22:40 +0800

    upgrade project to latest Android Studio version

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-export JAVA_HOME=/Users/wsong/Library/Java/JavaVirtualMachines/corretto-11.0.19/Contents/Home
+export JAVA_HOME=/opt/homebrew/Cellar/openjdk@17/17.0.7
 export ANDROID_SDK_ROOT=${HOME}/Library/Android/sdk
 export PATH=${ANDROID_SDK_ROOT}/platform-tools:${PATH}
 export ANDROID_NDK_ROOT=/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK
